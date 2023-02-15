048a82113a8efecfbe41e5db0a468966509e2c6b
sunnysimhadri <35062216+sunnysimhadri@users.noreply.github.com>
2023-02-15 10:13:56 +0530

    added installscript.sh

---
+++ installscript.sh
@@ -0,0 +1,22 @@
+# Install Java
+# Install Java
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y openjdk-13-jdk
+sudo apt-get -y update --fix-missing
+sudo apt-get install -y openjdk-13-jdk
+
+# Install tomcat
+sudo apt-get install -y  tomcat9
+
+#install mysql-server 5.6
+sudo apt-get -y install mysql-server-5.6 tree
+
+# Update and then close
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+if netstat -tulpen | grep 8080
+then
+exit 0
+fi
