e72af163d1af068457d6b60cb864cc9129bfc071
sunnysimhadri <35062216+sunnysimhadri@users.noreply.github.com>
2023-02-15 12:52:55 +0530

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -8,7 +8,8 @@ sudo apt-get install -y openjdk-13-jdk
 # Install tomcat
 sudo apt-get install -y tomcat9
 #install mysql-server 5.6
-sudo apt-get -y install mysql-server-5.6 tree
+sudo apt-get -y install mysql-server-5.7 
+sudo apt install tree
 # Update and then close
 sudo apt-get -y update
 sudo apt-get -y upgrade
